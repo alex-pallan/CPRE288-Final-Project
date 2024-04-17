9fd9768bdb9af41ee913fe374ee7aa1154d26d2a
alex-pallan <142113597+alex-pallan@users.noreply.github.com>
2024-04-16 19:22:54 -0500

    Add files via upload

---
+++ CprE288FinalProjectWorkspace/CPRE288 Final Project/main.c
@@ -0,0 +1,11 @@
+/*
+ * Final Project for CPRE288
+ * Iowa State University Spring 2024
+ *
+ * Authors: Jackson Collalti, Alex Pallan, Joe Ruhland, and Peter Yehl
+ */
+
+int main(void)
+{
+	return 0;
+}
